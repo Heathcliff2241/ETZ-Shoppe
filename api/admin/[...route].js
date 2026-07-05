9e62aae68f6f67ce415cda545056b7275e73964c
Cesar <cesaresmero2@gmail.com>
2026-07-05 09:21:17 +0100

    finalizing

--- api/admin/[...route].js
+++ api/admin/[...route].js
@@ -1,6 +1,18 @@
-import app, { initServer } from '../_lib/server.js';
+import express from 'express';
+import { initDb } from '../../server/db.js';
+import { adminRouter } from '../../server/routes/admin.js';
+
+const app = express();
+app.use(express.json());
+app.use('/', adminRouter);
 
 export default async function handler(req, res) {
-  await initServer();
+  await initDb();
+
+  const normalizedUrl = req.url?.startsWith('/api/admin')
+    ? req.url.replace(/^\/api\/admin/, '') || '/'
+    : req.url;
+
+  req.url = normalizedUrl || '/';
   return app(req, res);
 }
